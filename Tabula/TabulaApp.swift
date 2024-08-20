0b056bafb2d2dabee61348691680ac8bd9321ac1
Lucas <keyruu@web.de>
2024-08-20 11:31:35 +0200

    Initial Commit

---
+++ Tabula/TabulaApp.swift
@@ -0,0 +1,32 @@
+//
+//  TabulaApp.swift
+//  Tabula
+//
+//  Created by Lucas Rott on 20.08.24.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct TabulaApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
